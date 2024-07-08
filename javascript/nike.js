0717e7cf1e1fadba553ab2ea1881fd6d446bacac
shin-suhyeon <165615546+shin-suhyeon@users.noreply.github.com>
2024-07-08 16:40:34 +0900

    Add files via upload

---
+++ javascript/nike.js
@@ -0,0 +1,20 @@
+const airforce = new Swiper('.airforce',{
+    autoplay:{delay:3000},
+    direction:'horizontal',
+    slidesPerView: 3,
+    spaceBetween: 0,
+    loop:'true',
+})
+const Jordan = new Swiper('.Jordan',{
+    autoplay:{delay:4000},
+    direction:'horizontal',
+    slidesPerView: 3,
+    spaceBetween: 0,
+    loop:'true',
+})
+var swiper = new Swiper(".mySwiper", {
+    scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: false,
+    },
+});
